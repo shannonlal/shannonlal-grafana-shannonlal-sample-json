e403a069d9d5ffc2861334fd4853400ea7e505ca
Shannon Lal <shannonlal@hotmail.com>
2021-01-31 22:29:38 -0500

    Check in basic deploy script

--- src/DataSource.ts
+++ src/DataSource.ts
@@ -17,18 +17,25 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
   }
 
   async query(options: DataQueryRequest<MyQuery>): Promise<DataQueryResponse> {
+    console.log(`Options ${JSON.stringify(options)}`);
     const { range } = options;
     const from = range!.from.valueOf();
     const to = range!.to.valueOf();
 
+    function getRandomInt(max: number) {
+      return Math.floor(Math.random() * Math.floor(max));
+    }
+    console.log(`from: ${from} to: ${to}`);
     // Return a constant for each query.
     const data = options.targets.map(target => {
       const query = defaults(target, defaultQuery);
+      const value1 = getRandomInt(query.constant);
+      const value2 = getRandomInt(query.constant);
       return new MutableDataFrame({
         refId: query.refId,
         fields: [
           { name: 'Time', values: [from, to], type: FieldType.time },
-          { name: 'Value', values: [query.constant, query.constant], type: FieldType.number },
+          { name: 'Value', values: [value1, value2], type: FieldType.number },
         ],
       });
     });
